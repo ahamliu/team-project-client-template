806890f62f12eaef95aceac311f65e644a4d510d
70BOS <whnn97@gmail.com>
2017-04-23 12:01:49 -0400

    Migrate data to mock database on server

--- server/src/database.js
+++ server/src/database.js
@@ -1,6 +1,140 @@
 // Your startup's initial mock objects go here
 var initialData = {
+  listing: {
+    "1": {
+      "location": "UMass Amherst, Field dorm",
+      "date": Date.now(),
+      "animals": [1],
+      "title": "Pet listing",
+      "description": "famous cat on reddit",
+      "comments": [1],
+      "_id": 1,
+      "author": 1
+    }
+  },
+  animal: {
+    "1": {
+      "name": "Peanut",
+      "age": 999,
+      "type": "Cat",
+      "breed": "Main Coon",
+      "gender": "Male",
+      "characteristics": ["Fluffyyyyy", "CUTE"],
+      "imgURL": "img/sample-pet.jpg",
+      "_id": 1
+    },
+    "2": {
+      "name": "Randy",
+      "age": "Young",
+      "type": "Dog",
+      "breed": "Beagle",
+      "gender": "Male",
+      "location": "Cleveland, Ohio",
+      "characteristics": ["smart", "playful"],
+      "imgURL": "img/sample-pet.jpg",
+      "_id": 2
+    }
+  },
+  comment: {
+    "1": {
+      "author": "Kai",
+      "text": "HELLO I LIKE YOUR CAT hehe xd",
+      "_id": 1
+    }
+  },
+  user: {
+    "1": {
+      "name": "Amy Ham Liu",
+      "location": "Amherst, MA. USA",
+      "ratings": 1,
+      "imgURL": "img/sample-user.jpg",
+      "_id": 1
+    }
+  },
 
+
+
+
+
+
+  users: {
+    // This user has id "1".
+    "1": {
+      "_id": 1,
+      "fullName": "Someone",
+      "feed": 1
+    },
+    "2": {
+      "_id": 2,
+      "fullName": "Someone Else",
+      "feed": 2
+    },
+    "3": {
+      "_id": 3,
+      "fullName": "Another Person",
+      "feed": 3
+    },
+    // This is "you"!
+    "4": {
+      "_id": 4,
+      "fullName": "Cardie",
+      "time": "5 years ago",
+      "bio": "I hate javascript",
+      // ID of your feed.
+      "feed": 4,
+      "wishList": 1
+    }
+  },
+
+  "wishLists":{
+    "1": {
+      "_id": 4,
+      "contents": [1]
+    }
+  },
+  "wishListItems" :{
+    "1":{
+      "_id": 1,
+      "time": "Yesterday at 3:48pm",
+      "location": "Amherst, MA",
+      "content": "Want to adopt a poodle, hopefully near Amherst."
+    }
+  },
+  "feedItems": {
+    "1": {
+      "_id": 1,
+      "likeCounter": [
+        2, 3
+      ],
+      "type": "statusUpdate",
+      "contents": {
+        "author": 1,
+        "postDate": 1453668480000,
+        "location": "Austin, TX",
+        "contents": "Cardie is the best!"
+      }
+    }
+  },
+  // "feeds" collection. Feeds for each FB user.
+  "feeds": {
+    "4": {
+      "_id": 4,
+      "contents": [1]
+    },
+    "3": {
+      "_id": 3,
+      "contents": []
+    },
+    "2": {
+      "_id": 2,
+      "contents": []
+    },
+    "1": {
+      "_id": 1,
+      "contents": []
+    }
+  },
+  "querylist": []
 };
 
 var data;
